7522daafec0373a474993a6894006152155d9945
upidapi <videw@icloud.com>
2022-10-27 22:38:10 +0200

    (improved / added / cleaned upp) the raw_word_handler

--- Data/raw_word_handler/check_correct.py
+++ Data/raw_word_handler/check_correct.py
@@ -95,22 +95,6 @@ def check_correct(inp, answers):
     return True
 
 
-"""
-() => chain statement
-    # used manly to chain 'or statements' and 'text statements'
-    
-    (a, b, c) 
-    if you're at 'a' you have to go to next 'b'
-    you always have to go to the next index
-
-[] => or statement
-    (x, [a, b, c], y)
-    if you're at 'x' you have to go to 'a', 'b' or 'c'
-    if you're at 'a', 'b' or 'c' you have to go to y
-    
-    when entering you can go to any index (in the list) when entering
-    when exiting you have to go to after the list 
-"""
 
 data = (['är ', ''], '', 'blå')
 
---
+++ Data/raw_word_handler/make_anwser_sheet.py
@@ -0,0 +1,21 @@
+def make_answer_sheet():
+    """
+    makes an answer_sheet that the check_correct.py can use to check if a word was typed right
+
+    () => chain statement
+        # used manly to chain 'or statements' and 'text statements'
+
+        (a, b, c)
+        if you're at 'a' you have to go to next 'b'
+        you always have to go to the next index
+
+    [] => or statement
+        (x, [a, b, c], y)
+        if you're at 'x' you have to go to 'a', 'b' or 'c'
+        if you're at 'a', 'b' or 'c' you have to go to y
+
+        when entering you can go to any index (in the list) when entering
+        when exiting you have to go to after the list
+    """
+    pass
+
